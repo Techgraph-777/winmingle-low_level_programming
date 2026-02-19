b0a062b996202907520aaa4725f804978f5975ef
Techgraph-777 <certifiedcyberguru@gmail.com>
2026-02-18 21:14:48 -0800

    Add script to run C file through preprocessor

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  * Author: Emmanuel Chidera
+ *   * Program: WinMingle Community C Training
+ *    * Description: Prints a message using printf
+ *     */
+
+int main(void)
+{
+	    printf("with proper grammar, but the outcome is a piece of art\n");
+	        return (0);
+}
+
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+/* This is my test code
+ * 
+ * 
+ */
+int main(void)
+{ 
+	printf("Hello World\n");
+	return(0);
+}

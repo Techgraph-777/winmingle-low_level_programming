1fa94eeb9db96ab58000eb91b1595957960bc29a
Techgraph-777 <certifiedcyberguru@gmail.com>
2026-04-26 21:07:03 -0700

    0x02-functions_nested_loops: completed all tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  * print_alphabet - prints the alphabet in lowercase
+ *   *
+ *    * Return: void
+ *     *//**
+	    * print_alphabet - prints the alphabet in lowercase
+	    *  *
+	    *   * Return: void
+	    *    */
+
+void print_alphabet(void) {
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++) {
+	     _putchar(x);
+	}
+}

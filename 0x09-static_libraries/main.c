6047649a0af49ebe07504dc3d686ef3fe4d7caaa
Techgraph-777 <certifiedcyberguru@gmail.com>
2026-05-22 22:22:57 -0700

    Complete static libraries project

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - test library
+ *   
+ * Return: 0
+ */
+int main(void)
+{
+     _puts("\"WinMingle Community — Building the next generation of engineers\"\n\t- WinMingle");
+
+     return (0);
+}
